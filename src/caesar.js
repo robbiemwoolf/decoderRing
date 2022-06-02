f2f0b6234bde89abb62ee6775f1e086ec11940d4
robbiemwoolf <102851621+robbiemwoolf@users.noreply.github.com>
2022-06-02 09:25:09 -0700

    initial completed caesar function

--- src/caesar.js
+++ src/caesar.js
@@ -4,11 +4,45 @@
 // of the anonymous function on line 6
 
 const caesarModule = (function () {
-  // you can add any code you want within this function scope
-
   function caesar(input, shift, encode = true) {
-    // your solution code here
-  }
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    let output = "";
+    let message = input.toLowerCase(); // ignores capital letters
+
+    // invalid input and shift values
+    if (shift === 0 || shift < -25 || shift > 25 || !shift || !input) { 
+        return false;
+    }
+
+    for (let i = 0; i < message.length; i++) {
+        for (let j = 0; j < alphabet.length; j++) {
+            if (!alphabet.includes(message[i])) { //finds spaces and special characters
+                output += message[i]; // adds them to output
+                break
+            }
+            
+            if (alphabet[j] === message[i]) { // encoding shift
+                let shifted = j + shift;
+                if (!encode) {   // decoding shift
+                    shifted = j - shift;
+                }
+                
+                // account for beginning and end of alphabet
+                if (shifted < 0) {
+                    shifted += 26;
+                    output += alphabet[shifted];
+                } else if (shifted > 25) {
+                    shifted -= 26;
+                    output += alphabet[shifted];
+                } else {
+                    output += alphabet[shifted];
+                }
+            }
+        }
+    }
+    return output;
+}
+
 
   return {
     caesar,
@@ -16,3 +50,4 @@ const caesarModule = (function () {
 })();
 
 module.exports = { caesar: caesarModule.caesar };
+
